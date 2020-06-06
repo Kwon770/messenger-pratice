4dd8e0e51dc8eb6ba524365dbe1adfe9907c591b
Kwon770 <sckwon770@gmail.com>
2020-06-06 15:53:09 +0900

    feat: Sending Messages and KeyboardAvoidingView

--- app/chat.tsx
+++ app/chat.tsx
@@ -1,12 +1,88 @@
-import React from "react";
-import { ApolloProvider } from "react-apollo-hooks";
-import client from "./apollo";
-import Chat from "./chat";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "react-apollo-hooks";
+import withSuspense from "./withSuspense";
 
-export default function App() {
+const GET_MESSAGES = gql`
+  query messages {
+    messages {
+      id
+      text
+    }
+  }
+`;
+
+const SEND_MESSAGE = gql`
+  mutation sendMessage($text: String!) {
+    sendMessage(text: $text) {
+      id
+      text
+    }
+  }
+`;
+
+function Chat() {
+  const [message, setMessage] = useState("");
+  const [sendMessageMutation] = useMutation(SEND_MESSAGE, {
+    variables: {
+      text: message,
+    },
+    refetchQueries: () => [{ query: GET_MESSAGES }],
+  });
+  const { data, error } = useQuery(GET_MESSAGES, { suspend: true });
+  const onChangeText = (text: string) => setMessage(text);
+  const onSubmit = async () => {
+    if (message === "") {
+      return;
+    }
+
+    try {
+      await sendMessageMutation();
+      setMessage("");
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
-    <ApolloProvider client={client}>
-      <Chat />
-    </ApolloProvider>
+    <KeyboardAvoidingView style={{ flex: 1 }} enabled behavior="padding">
+      <ScrollView
+        contentContainerStyle={{
+          paddingVertical: 50,
+          flex: 1,
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        {data.messages.map((message: any) => (
+          <View key={message.id} style={{ marginBottom: 10 }}>
+            <Text>{message.text}</Text>
+          </View>
+        ))}
+        <TextInput
+          placeholder="Type a message"
+          style={{
+            marginTop: 50,
+            width: "90%",
+            borderRadius: 10,
+            paddingVertical: 15,
+            paddingHorizontal: 10,
+            backgroundColor: "#f2f2f2",
+          }}
+          returnKeyType="send"
+          value={message}
+          onChangeText={onChangeText}
+          onSubmitEditing={onSubmit}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
+
+export default withSuspense(Chat);
